166091e77cf3ac0847bbd7be83c882c31fbad9eb
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-09-20 09:35:35 +0300

    scroll almost done

--- src/components/eventList/eventList.jsx
+++ src/components/eventList/eventList.jsx
@@ -10,24 +10,47 @@ function EventList() {
     const [page, setPage] = useState(1)
     const [sortBy, setSortBy] = useState('title')
     const [sortOrder, setSortOrder] = useState('asc')
+    const [hasMore, setHasMore] = useState(false)
 
     useEffect(() => {
         async function getEvents() {
             axios.defaults.baseURL = 'http://localhost:14000'
-            const result = await axios.get('/events',{params: {perPage: 10, page, sortBy, sortOrder}})
+            const result = await axios.get('/events',{params: {perPage: 15, page, sortBy, sortOrder}})
             if(page === 1) {
                 setEvents(result.data.data.data)
             } else {
+                if(result.data.data.data.length === 0) return
             setEvents(prevEvents => [...prevEvents, ...result.data.data.data]);
+            }
+            if(result.data.data.hasNextPage) {
+                setHasMore(true)
+            } else {
+                setHasMore(false)
             }
         }
         getEvents()
     }, [page, sortBy, sortOrder])
 
+    useEffect(() => {
+        function handleScroll() {
+            if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 50 && hasMore) {
+                setPage(prevPage => prevPage + 1);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [ hasMore]);
+
     function onSort(values) {
         setSortBy(values.sortBy)
         setSortOrder(values.sortOrder)
+        setPage(1)
     }
+    
 
     return (
         <div>
