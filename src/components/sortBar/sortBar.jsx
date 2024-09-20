b022ce8ffbbbd60dd79bb006f06ed2faabcefe08
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-09-20 14:02:33 +0300

    only css need to be done

--- src/components/sortBar/sortBar.jsx
+++ src/components/sortBar/sortBar.jsx
@@ -1,5 +1,7 @@
 import { Formik, Form, Field } from "formik";
 
+import css from './sortBar.module.css'
+
 function SortBar({onSort}) {
 
     const initial = {
@@ -13,24 +15,33 @@ function SortBar({onSort}) {
 
     return (
 
-        <div>
+        <div className={css.sortBar}>
+            <p className={css.title}>Sortation</p>
             <Formik
             initialValues={initial}
             onSubmit={submitHandler}
             >
-                <Form>
-                    <Field as='select' name='sortBy'>
+                <Form className={css.formContainer}>
+                    <div className={css.form}>
+                    <div className={css.selectBox}>
+                    <label htmlFor="sortBy">Sort events by...</label>
+                    <Field className={css.option} id='sortBy' as='select' name='sortBy'>
                         <option value=""></option>
                         <option value="organizer" label="Sort by organizer"></option>
                         <option value="title" label="Sort by title"></option>
                         <option value="eventDate" label="Sort by event date"></option>
                     </Field>
-                    <Field as='select' name='sortOrder'>
+                    </div>
+                    <div className={css.selectBox}>
+                    <label htmlFor="sortOrder">Sort order...</label>
+                    <Field className={css.option} id='sortOrder' as='select' name='sortOrder'>
                         <option value=""></option>
                         <option value="asc" label="ASC"></option>
                         <option value="desc" label="DESC"></option>
                     </Field>
-                    <button type="submit" >Sort</button>
+                    </div>
+                    </div>
+                    <button className={css.btn} type="submit" >Sort</button>
                 </Form>
             </Formik>
         </div>
